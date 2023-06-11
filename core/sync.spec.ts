a0414e97a79901d556a638c8e436044f37801500
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-06-11 10:48:08 +0200

    test: use promise correctly

--- core/sync.spec.ts
+++ core/sync.spec.ts
@@ -1,23 +1,35 @@
 import { before, beforeEach, describe, test as it } from 'node:test'
 import {
-	arrayContaining,
+	arrayMatching,
 	check,
 	objectMatching,
 	undefinedValue,
 } from 'tsmatchers'
 import { ulid } from 'ulid'
-import type { CoreEvent } from './CoreEvent'
-import { CoreEventType } from './CoreEventType'
-import type { UserAuthContext } from './auth'
-import { notifier } from './notifier'
-import type { DbContext } from './persistence/DbContext'
-import { createOrganization } from './persistence/createOrganization'
-import { createProject } from './persistence/createProject'
-import { createStatus } from './persistence/createStatus'
-import { createSync } from './persistence/createSync'
-import { getStatusInSync } from './persistence/getStatusInSync'
-import { createTestDb } from './test/createTestDb'
-import { testDb } from './test/testDb'
+import type { CoreEvent } from './CoreEvent.js'
+import { CoreEventType } from './CoreEventType.js'
+import type { ProblemDetail } from './ProblemDetail.js'
+import type { UserAuthContext } from './auth.js'
+import { notifier } from './notifier.js'
+import type { DbContext } from './persistence/DbContext.js'
+import { createOrganization } from './persistence/createOrganization.js'
+import { createProject } from './persistence/createProject.js'
+import { createStatus } from './persistence/createStatus.js'
+import { createSync } from './persistence/createSync.js'
+import { getStatusInSync } from './persistence/getStatusInSync.js'
+import { createTestDb } from './test/createTestDb.js'
+import { testDb } from './test/testDb.js'
+
+const isNotAnError = <Result>(
+	res: { error: ProblemDetail } | Result,
+): Result => {
+	check(res).is(
+		objectMatching({
+			error: undefinedValue,
+		}),
+	)
+	return res as Result
+}
 
 describe('sync', async () => {
 	const { TableName, db } = testDb()
@@ -42,49 +54,37 @@ describe('sync', async () => {
 
 		// Given there is a project with status
 		beforeEach(async () => {
-			check(
+			isNotAnError(
 				await createOrganization(dbContext, notify)(
 					{ id: organizationId, name: `Organization ${organizationId}` },
 					user,
 				),
-			).is(
-				objectMatching({
-					error: undefinedValue,
-				}),
 			)
 			return Promise.all(
 				projectIds.map(async (projectId) => {
-					check(
+					isNotAnError(
 						await createProject(dbContext, notify)(
 							{ id: projectId, name: `Project ${projectId}` },
 							user,
 						),
-					).is(
-						objectMatching({
-							error: undefinedValue,
-						}),
 					)
-					return Promise.all([
-						...[1, 2, 3, 4].map((i) => async () => {
+					return Promise.all(
+						[1, 2, 3, 4].map(async (i) => {
 							const statusId = ulid()
 							createdStatus[projectId] = [
 								...(createdStatus[projectId] ?? []),
 								statusId,
 							]
-							check(
+							isNotAnError(
 								await createStatus(dbContext, notify)(
 									statusId,
 									projectId,
 									`Status ${i} for project ${projectId}`,
 									user,
 								),
-							).is(
-								objectMatching({
-									error: undefinedValue,
-								}),
 							)
 						}),
-					])
+					)
 				}),
 			)
 		})
@@ -95,30 +95,21 @@ describe('sync', async () => {
 			const events: CoreEvent[] = []
 			on(CoreEventType.SYNC_CREATED, (e) => events.push(e))
 
+			console.log(createdStatus)
+
 			const syncId = ulid()
-			const res = await createSync(dbContext, notify)(
-				syncId,
-				[projectA, projectB],
-				'My sync',
-				user,
+			isNotAnError(
+				await createSync(dbContext, notify)(
+					syncId,
+					[projectA, projectB],
+					'My sync',
+					user,
+				),
 			)
 
-			check(res).is(
-				objectMatching({
-					error: undefinedValue,
-				}),
+			const statusInSync = isNotAnError(
+				await getStatusInSync(dbContext, user)(syncId),
 			)
-			check(events[0]).is(
-				objectMatching({
-					id: syncId,
-					type: CoreEventType.SYNC_CREATED,
-					projects: [projectA, projectB],
-					title: 'My sync',
-					author: '@alex',
-				}),
-			)
-
-			const statusInSync = await getStatusInSync(dbContext, user)(syncId)
 
 			const expectedStatusIds: string[] = [
 				...(createdStatus[projectA] ?? []),
@@ -127,11 +118,9 @@ describe('sync', async () => {
 
 			console.log(statusInSync)
 
-			check(statusInSync).is(
-				objectMatching({
-					status: arrayContaining(expectedStatusIds.map((id) => ({ id }))),
-				}),
-			)
+			const createdIds = statusInSync.status.map(({ id }) => id)
+
+			check(createdIds).is(arrayMatching(expectedStatusIds))
 		})
 	})
 })
