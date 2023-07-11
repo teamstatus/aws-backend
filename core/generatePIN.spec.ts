61bee7813a02c5fe5d74e30d70706683df5c0a9d
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-07-12 00:00:50 +0200

    test: ensure PINs are unique

--- core/generatePIN.spec.ts
+++ core/generatePIN.spec.ts
@@ -1,8 +1,10 @@
 import { describe, test as it } from 'node:test'
-import { check, stringMatching } from 'tsmatchers'
+import { check, not, stringMatching } from 'tsmatchers'
 import { generatePIN } from './generatePIN.js'
 
 describe('generatePIN()', () => {
 	void it('should generate an 8 digit PIN', () =>
 		check(generatePIN()).is(stringMatching(/^[0-9]{8}$/)))
+	void it('should create unique PINs', () =>
+		check(generatePIN()).is(not(stringMatching(generatePIN()))))
 })
